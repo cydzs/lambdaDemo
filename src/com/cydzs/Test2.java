f688a52fd6d252149d4107f0441fed73a83a0bb7
cydzs <maochun1129@qq.com>
2021-04-24 14:38:58 +0800

    测试

---
+++ src/com/cydzs/Test2.java
@@ -0,0 +1,13 @@
+package com.cydzs;
+
+public class Test2 {
+
+    public static void main(String[] args) {
+        int a =100;
+        for (int i = 0; i < 100; i++) {
+            a+=i;
+            System.out.println(a);
+        }
+
+    }
+}
